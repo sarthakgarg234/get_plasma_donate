0546eb9290721c75fd6e37b91370b22ff7453832
sarthakgarg234 <sarthakgarg234@gmail.com>
2021-04-24 14:45:25 +0530

    new changes

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 define("base_root", "http://localhost/get_plasma_donate/");
-$con=mysqli_connect("localhost", "root", "getPlasma@#234aws", "get_plasma_donate");
+$con=mysqli_connect("localhost", "root", "", "");
 if(mysqli_connect_errno())
 {
 echo "Connection Fail".mysqli_connect_error();
